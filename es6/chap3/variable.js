d72713e4d8c999eddaa25656aad7a9940c31d4c7
Hee-jin506 <gmlwls35220@gmail.com>
2020-11-10 02:21:51 +0900

    chap 3

---
+++ es6/chap3/variable.js
@@ -0,0 +1,44 @@
+let currentTempC = 22;
+currentTempC2 = 22.5;
+
+let targetTempC;
+let targetTempC, room1 = "conference_room_a", room2 = "lobby";
+
+const ROOM_TEMP_C = 21.5, MAX_TEMP_C = 30;
+
+let room1 = "conference_room_a";
+let currentRoom = room1;
+currentRoom = conference_room_a; // 에러
+
+let count = 10;         // 더블
+const blue = 0x0000ff;  // 16진수
+const umask = 0o0022;   // 8진수
+const roomTemp = 21.5;  // 10진수
+const c = 3.0e6;        // 지수 표기법
+const inf = Infinity;
+const ninf = -Infinity;
+const nan = NaN;        // '숫자가 아님'
+
+const small =  Number.EPSILON;          // 1을 더했을 때 1과 구분하는 결과를 만들 수 있는 가장 작은 값 2.2e-16
+const bigInt = Number.MAX_SAFE_INTEGER; // 표현할 수 있는 가장 큰 정수
+const max = Number.MAX_VALUE;           // 표현할 수 있느 가장 큰 숫자
+const minInt = Number.MIN_SAFE_INTEGER; // 표현할 수 있는 가장 작은 정수
+const min = Number.MIN_VALUE;           // 표현할 수 있는 가장 작은 숫자
+const nInf = Number.NEGATIVE_INFINITY   // -Infinity
+const nan = Number.NaN;                 // NaN
+const inf = Number.POSITIVE_INFINITY    // Infinity
+
+const dialog = 'Sam looked up, and said "hello, old friend!", as Max walked in.';
+const imperative = "Don't do that!";
+
+const dialog = "Sam looked up and said "don't do that!" to Max";
+
+const dialog1 = "He looked up and said \"don't do that\" to Max";
+const dialog2 = 'He looked up and said "dont\'t do that" to Max';
+
+const s = "In JavaScript, use \\ as an escape character in strings";
+
+let currentTemp = 19.5;
+const message = "The current temperature is " + currentTemp + "\u00B0C";
+const message2 = `The current temperature is ${currentTemp}\u00B0C`;
+
